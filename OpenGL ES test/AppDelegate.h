60d9734edf1f5077eba6398cfd02d685a4701d7e
Stanley Wang <misewang@gmail.com>
2017-05-25 14:32:59 +0800

    Initial Commit

---
+++ OpenGL ES test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGL ES test
+//
+//  Created by Stanley Wang on 2017/5/25.
+//  Copyright © 2017年 Stanley Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGL ES test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL ES test
+//
+//  Created by Stanley Wang on 2017/5/25.
+//  Copyright © 2017年 Stanley Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
